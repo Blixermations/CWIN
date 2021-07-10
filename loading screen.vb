1c844e4f8cce9439f091d114315574ac52cb0f22
BLX <70741560+Blixermations@users.noreply.github.com>
2021-07-10 12:30:44 +1000

    0.7 REALSE FILES
    These are the realise files for 0.7

--- loading screen.vb
+++ loading screen.vb
@@ -1,4 +1,5 @@
 ﻿Public Class loading_screen
+    Public DEVMODE As Boolean = True
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         ProgressBar1.Increment(1)
         If ProgressBar1.Value = 100 Then
@@ -9,8 +10,17 @@
     End Sub
 
     Private Sub loading_screen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DEVMODE_Button()
         Cursor.Hide()
         Timer1.Start()
 
+    End Sub
+
+    Private Sub DEVMODE_Button()
+        If DEVMODE = True Then
+            Form1.Show()
+            MsgBox("DEVMODE has been activated.")
+            Me.Close()
+        End If
     End Sub
 End Class
